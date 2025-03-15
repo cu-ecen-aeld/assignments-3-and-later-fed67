a343a18bfb808b252d98b64fea8e2ca3995e9e7f
fed67 <you@example.com>
2025-03-15 18:12:27 +0100

    drivertest works

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -18,13 +18,27 @@
 #include <linux/cdev.h>
 #include <linux/fs.h> // file_operations
 #include "aesdchar.h"
+#include <linux/rwlock.h>
+
+#include "aesd-circular-buffer.h"
 int aesd_major =   0; // use dynamic major
 int aesd_minor =   0;
 
-MODULE_AUTHOR("Your Name Here"); /** TODO: fill in your name **/
+MODULE_AUTHOR("fed67"); /** TODO: fill in your name **/
 MODULE_LICENSE("Dual BSD/GPL");
 
+static DEFINE_RWLOCK(myrwlock);
+
 struct aesd_dev aesd_device;
+
+struct aesd_circular_buffer* my_ring_buf;
+
+size_t read_complete = 0;
+
+
+//write
+char* buffer = NULL;
+size_t buffer_length = 0;
 
 int aesd_open(struct inode *inode, struct file *filp)
 {
@@ -32,6 +46,8 @@ int aesd_open(struct inode *inode, struct file *filp)
     /**
      * TODO: handle open
      */
+    read_complete = 0;
+
     return 0;
 }
 
@@ -41,18 +57,62 @@ int aesd_release(struct inode *inode, struct file *filp)
     /**
      * TODO: handle release
      */
+
     return 0;
 }
 
+// Buffer must be read fully
+// otherwise it changes
 ssize_t aesd_read(struct file *filp, char __user *buf, size_t count,
                 loff_t *f_pos)
 {
     ssize_t retval = 0;
     PDEBUG("read %zu bytes with offset %lld",count,*f_pos);
+
+    size_t offset = *f_pos;
+    loff_t size = min(get_total_size(my_ring_buf), count);
     /**
      * TODO: handle read
      */
-    return retval;
+
+    //file was read completly
+    if(read_complete == 1) {
+        return 0;
+    }
+
+    char* buffer_loc = (char*) kmalloc(sizeof(char)*size, GFP_KERNEL);
+        if(buffer_loc == NULL) {
+        return -ENOMEM;
+    }
+
+    // PDEBUG("in_offs %u out_offs %u full %i \n", my_ring_buf->in_offs, my_ring_buf->out_offs, my_ring_buf->full );
+
+    unsigned int flags = 0;
+    read_lock_irqsave(&myrwlock, flags);
+
+    size_t num_data_read = 0;
+    for(size_t i = 0; i < size; ++i ) {
+        size_t entry_offset_byte_rtn = 0;
+
+        struct aesd_buffer_entry* ptr = aesd_circular_buffer_find_entry_offset_for_fpos(my_ring_buf, offset+i, &entry_offset_byte_rtn);
+
+        if(ptr == NULL) {
+            read_unlock_irqrestore(&myrwlock, flags);
+            return num_data_read; // returns EOF when using cat
+        }
+        PDEBUG("val >%c< \n", ptr->buffptr[entry_offset_byte_rtn] );
+        buffer_loc[i] = ptr->buffptr[entry_offset_byte_rtn];
+        num_data_read += 1;
+    }
+
+    read_unlock_irqrestore(&myrwlock, flags);
+
+    if(copy_to_user(buf, buffer_loc, size))
+        return -EFAULT; 
+
+    read_complete = 1;
+
+    return size;
 }
 
 ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
@@ -63,14 +123,110 @@ ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
     /**
      * TODO: handle write
      */
-    return retval;
+
+     unsigned int flags = 0;
+     size_t size = count;
+
+     char* buffer_loc = (char*) kmalloc(sizeof(char)*count, GFP_KERNEL);
+     if(buffer_loc == NULL) {
+        return -ENOMEM;
+     }
+
+     if(copy_from_user(buffer_loc, (int __user *)buf, count)) {
+        kfree(buffer_loc);
+        return -EFAULT;
+     }
+
+     //handle when not receiving a '\n' at the end
+     if(buffer_loc[count-1] != '\n') {
+        if(buffer != NULL) {
+            void* result = (char*) krealloc(buffer, sizeof(char)*(count+buffer_length), GFP_KERNEL); //extend the buffer size
+            if(result == NULL)
+                return -ENOMEM;
+
+            memcpy(buffer+buffer_length, buffer_loc, count);
+            kfree(buffer_loc);
+            buffer_length += count;
+        } else {
+            buffer = buffer_loc;
+            buffer_length = count;
+        }
+        return count;
+     }
+
+     if(buffer != NULL) {
+        // PDEBUG("Merging data\n");
+        void* result = (char*) krealloc(buffer, sizeof(char)*(count+buffer_length), GFP_KERNEL); //extend the buffer size
+        if(result == NULL)
+            return -ENOMEM;
+
+        memcpy(buffer+buffer_length, buffer_loc, count);
+        kfree(buffer_loc);
+        buffer_loc = buffer;
+        buffer = NULL;
+        buffer_length += count;
+
+        size = buffer_length; // write all data
+     }
+
+     for(int i = 0; i < size; ++i) {
+        // buffer_loc[i] = buf[i];
+        PDEBUG("Write character: <%c> \n", buffer_loc[i]);
+     }
+
+     struct aesd_buffer_entry *add_entry = (struct aesd_buffer_entry *) kmalloc(sizeof(struct aesd_buffer_entry), GFP_KERNEL);
+     if(add_entry == NULL) {
+        kfree(buffer_loc);
+        return -ENOMEM;
+     }
+
+     add_entry->buffptr = buffer_loc;
+     add_entry->size = size;
+
+     write_lock_irqsave(&myrwlock, flags);
+
+     //free entry
+     if(my_ring_buf->full) {
+        kfree(my_ring_buf->entry[my_ring_buf->in_offs].buffptr);
+     }
+
+     aesd_circular_buffer_add_entry(my_ring_buf, add_entry);
+
+     kfree(add_entry);
+
+     write_unlock_irqrestore(&myrwlock, flags);
+
+    
+    return count;
 }
+
+//support llseek to determine the size of the buffer, needed for aesdsocket
+static loff_t aesd_llseek(struct file *file, loff_t offset, int whence) {
+    loff_t pos = 0;
+
+    switch (whence) {
+        // SEEK_CUR and set are handeled equally
+        case SEEK_SET: 
+        case SEEK_CUR:  
+            pos = offset;
+            break;
+        case SEEK_END:  
+            pos = get_total_size(my_ring_buf) + offset;
+            break;
+        default:
+            return -EINVAL;
+    }
+
+    return pos;
+}
+
 struct file_operations aesd_fops = {
     .owner =    THIS_MODULE,
     .read =     aesd_read,
     .write =    aesd_write,
     .open =     aesd_open,
     .release =  aesd_release,
+    .llseek =   aesd_llseek,
 };
 
 static int aesd_setup_cdev(struct aesd_dev *dev)
@@ -111,6 +267,21 @@ int aesd_init_module(void)
     if( result ) {
         unregister_chrdev_region(dev, 1);
     }
+
+
+    my_ring_buf = (struct aesd_circular_buffer*) kmalloc(10*sizeof(struct aesd_circular_buffer), GFP_KERNEL);
+    my_ring_buf->in_offs = 0;
+    my_ring_buf->out_offs = 0;
+    my_ring_buf->full = false;
+
+    if( my_ring_buf == NULL) {
+        return -ENOMEM;
+    }
+    aesd_circular_buffer_init(my_ring_buf);
+
+    buffer = NULL;
+    buffer_length = 0;
+
     return result;
 
 }
@@ -125,10 +296,12 @@ void aesd_cleanup_module(void)
      * TODO: cleanup AESD specific poritions here as necessary
      */
 
+    for(size_t i = 0; i < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; ++i) {
+        kfree(my_ring_buf->entry[i].buffptr);
+    }
+
     unregister_chrdev_region(devno, 1);
 }
-
-
 
 module_init(aesd_init_module);
 module_exit(aesd_cleanup_module);
